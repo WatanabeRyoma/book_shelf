71633217934f22d92f216a06d151423197d48313
ケルマイ <gyogyooon@gmail.com>
2019-05-04 11:13:10 +0900

    削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,12 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "書籍を削除しました。"
+  end
   private
 
   def book_params
