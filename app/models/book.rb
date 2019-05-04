08c024a1706a965e9ca95a5be192645ed5644f5e
ケルマイ <gyogyooon@gmail.com>
2019-05-04 17:45:46 +0900

    レビュー投稿機能作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 
 class Book < ApplicationRecord
 belongs_to :category
-  has_one_attached :image
+has_many :reviews, dependent: :destroy
+has_one_attached :image
 attribute :new_image
 
   validates :title, presence: true, length: { maximum: 50 }
